58223b1616c68519d8f6be9edb9f8762800c6972
derek21298 <derek21298@gmail.com>
2019-02-27 20:18:00 -0500

    Lab4 files

--- lab4/lab4_main.c
+++ lab4/lab4_main.c
@@ -7,97 +7,177 @@
 #define PRESSED 1
 #define RELEASED 0
 
+void set_up_pins(void);
+struct button_properties button_check(int button_a, int button_b, int button_c, struct button_properties);
+
+// Holding the properties of button in struct to avoid global variable
+struct button_properties {
+	int time_delay;
+	int direction;
+};	
+
+
 int main(int argc, char** argv) {
 
-	int i;
-	int button_a, button_b;
-	int time_delay = 1024;
-	char buffer[256];	
-	char sys_buffer[256];
+	int button_a, button_b, button_c;
+	struct button_properties s;
+	s.time_delay = 1024;
+	s.direction = 1;
+	
+	set_up_pins();
 
-	pinMode(18, INPUT);
-	pinMode(20, INPUT);
-
-	pullUpDnControl(18, PUD_UP);
-	pullUpDnControl(20, PUD_UP);
-
-	system("./lab4_rotate 1024");
+	system("pkill lab4_rotate");
+	system("./lab4_rotate 1024&");
 
 	while(1){
 
-		button_a = digitalRead(18);
-		button_b = digitalRead(20);
+		button_a = digitalRead(25);
+		button_b = digitalRead(27);
+		button_c = digitalRead(28);		
 
-		if((button_a == PRESSED) && (button_b == RELEASED)) {
-			
-			// Make sure the button has been pressed for 50 us
-			delay(50);
-			if(button_a == PRESSED) {
-				printf("BUTTON A IS PRESSED\n");
-				
-				// Dont let time-delay be less than 32
-				if(time_delay > 32) {
-					time_delay /= 2;
-				}
-			
-				printf("Time delay is %d us\n", time_delay);
-			
-				// String Manipulation
-				snprintf(buffer, 10, "%d", time_delay);
-				strcpy(sys_buffer, "./lab4_rotate ");
-				strcat(sys_buffer, buffer);
-			
-				// Kill the previous process and start a new one
-				system("pkill lab4_rotate");
-				system(sys_buffer);
-			}
-			
-			// Wait until the button is relased
-			while(button_a == RELEASED);
-			printf("BUTTON A IS RELEASED\n");
-		}
-
-		if((button_b == PRESSED) && (button_a == RELEASED) ) {
-			
-			// Make sure the button has been pressed for 50 us
-			delay(50);
-			if(button_b == PRESSED) {
-				
-				printf("BUTTON B IS PRESSED\n");
-				// Dont let time-delay be greater than 32
-				if(time_delay < 1024) {
-					time_delay *= 2;
-				}
-					
-				printf("Time delay is %d us\n", time_delay);
-
-				// String Manipulation
-				snprintf(buffer, 10, "%d", time_delay);
-				strcpy(sys_buffer, "./lab4_rotate ");
-				strcat(sys_buffer, buffer);
-			
-				// Kill the previous process and start a new one
-				system("pkill lab4_rotate");
-				system(sys_buffer);
-			}
-			
-			// Wait until the button is relased
-			while(button_b == RELEASED);
-			printf("BUTTON B IS RELEASED\n");
-		}
-
-				
-		if((button_b == PRESSED) && (button_a == PRESSED) ) {
-			
-			// Make sure the button has been pressed for 50 us
-			delay(50);
-			if((button_b == PRESSED) && (button_a == PRESSED)) {
-			
-				// Kill the previous process
-				system("pkill lab4_rotate");
-				return 0;
-			}	
-		}
+		printf("BUTTON A VALUE: %d\n", button_a);	
+		printf("BUTTON B VALUE: %d\n", button_b);	
+		
+		s = button_check(button_a, button_b, button_c, s);
+		
 	}
 
+}
+
+void set_up_pins(void) {
+
+	wiringPiSetup();
+	pinMode(25, INPUT);
+	pinMode(27, INPUT);
+
+	pullUpDnControl(25, PUD_DOWN);
+	pullUpDnControl(27, PUD_DOWN);
+}
+
+struct button_properties button_check(int button_a, int button_b, int button_c, struct button_properties s) {
+
+	char buffer[256];
+	char sys_buffer[256];
+
+	if((button_a == PRESSED) && (button_b == RELEASED) && (button_c == RELEASED)) {			
+		// Make sure the button has been pressed for 50 us
+		delay(150);
+		if(button_a == PRESSED) {
+			printf("BUTTON A IS PRESSED\n");
+			
+			if((s.time_delay == 32) && (s.direction == 1)) {
+				s.direction = 2;
+			}
+			else if((s.time_delay == 32) && (s.direction == 2)) {
+				s.direction = 1;
+			}
+			// Dont let time-delay be less than 32
+			if(s.time_delay > 32) {
+				s.time_delay /= 2;
+			}
+		
+			printf("Time delay is %d us\n", s.time_delay);
+		
+			snprintf(buffer, 10, "%d", s.time_delay);
+			strcpy(sys_buffer, "./lab4_rotate ");
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer," ");
+			snprintf(buffer, 10, "%d", s.direction);
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer, "&");
+			printf("I made it!\n");
+		
+			// Kill the previous process and start a new one
+			system("pkill lab4_rotate");
+			system(sys_buffer);
+		}
+			
+		// Wait until the button is relased
+		printf("Direction is = %d\n", s.direction);
+		while(button_a == PRESSED) {
+			button_a = digitalRead(25);
+			button_b = digitalRead(27);
+			if(button_b == PRESSED) {
+				system("pkill lab4_rotate -f");
+				system("pkill lab4_main -f");
+			}
+		}
+		printf("BUTTON A IS RELEASED\n");
+	}
+
+	if((button_b == PRESSED) && (button_a == RELEASED) && (button_c == RELEASED)) {
+		
+		// Make sure the button has been pressed for 50 us
+		delay(150);
+		if(button_b == PRESSED) {
+			
+			if((s.time_delay == 1024) && (s.direction == 1)) {
+				s.direction = 2;
+			}
+			else if((s.time_delay == 1024) && (s.direction == 2)) {
+				s.direction = 1;
+			}
+			printf("BUTTON B IS PRESSED\n");
+			// Dont let time-delay be greater than 32
+			if(s.time_delay < 1024) {
+				s.time_delay *= 2;
+			}
+					
+			printf("Time delay is %d us\n", s.time_delay);
+			
+			snprintf(buffer, 10, "%d", s.time_delay);
+			strcpy(sys_buffer, "./lab4_rotate ");
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer," ");
+			snprintf(buffer, 10, "%d", s.direction);
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer, "&");
+			printf("I made it!\n");
+		
+			// Kill the previous process and start a new one
+			system("pkill lab4_rotate -f");
+			system(sys_buffer);
+		}
+			
+		// Wait until the button is relased
+		while(button_b == PRESSED) {
+			button_b = digitalRead(27);
+			button_a = digitalRead(25);
+			if(button_a == PRESSED) {
+				system("pkill lab4_rotate -f");
+				system("pkill lab4_main -f");
+			}
+		}
+		printf("BUTTON B IS RELEASED\n");
+	}
+		
+	if((button_c == PRESSED) && (button_a == RELEASED) && (button_b == RELEASED)) {
+					
+		// Make sure the button has been pressed for 50 us
+		delay(150);
+		if(button_c == PRESSED) {
+			s.time_delay = 1024;
+			s.direction = 1;
+					
+			printf("Time delay is %d us\n", s.time_delay);
+			
+			snprintf(buffer, 10, "%d", s.time_delay);
+			strcpy(sys_buffer, "./lab4_rotate ");
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer," ");
+			snprintf(buffer, 10, "%d", s.direction);
+			strcat(sys_buffer, buffer);
+			strcat(sys_buffer, "&");
+			printf("I made it!\n");
+		
+			// Kill the previous process and start a new one
+			system("pkill lab4_rotate -f");
+			system(sys_buffer);
+		}
+
+		// Wait until the button is relased
+		while(button_c == PRESSED);
+		printf("BUTTON C IS RELEASED\n");
+	}
+	return s;
 }
--- lab4/lab4_rotate.c
+++ lab4/lab4_rotate.c
@@ -1,3 +1,13 @@
+/*
+*	ECE 477 Lab 4
+*	Authors: Alex Barnett, Derek Haas, Dustin Knight
+*	Date: 3/1/2019
+*	File: lab4_rotate.c
+*	Function: 	Iterate through 8 LEDs (GPIO0 through GPIO7) from left-to-right
+*			or right-to-left
+*/
+
+
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,9 +16,12 @@
 #define PRESSED 1
 #define RELEASED 0
 
-int main(int argc, char** argv) {
 
-	int i;
+void set_up_pins(void);
+void moving_forward(int time_delay);
+void moving_backward(int time_delay);
+
+int main(int argc, char** argv) {
 
 	if(argc < 2) {
 		printf("Invalid Use Of Program!\n");
@@ -16,59 +29,71 @@ int main(int argc, char** argv) {
 	}
 
 	int time_delay = atoi(argv[1]);
+	int direction = atoi(argv[2]);	
+	int i;
 
-	wiringPiSetup();
-
-	pinMode(0, OUTPUT);
-	pinMode(1, OUTPUT);
-	pinMode(2, OUTPUT);
-	pinMode(3, OUTPUT);
-	pinMode(4, OUTPUT);
-	pinMode(5, OUTPUT);
-	pinMode(6, OUTPUT);
-	pinMode(7, OUTPUT);
-
-	pinMode(15, INPUT);
-	pinMode(16, INPUT);
-
-	pullUpDnControl(15, PUD_UP);
-	pullUpDnControl(16, PUD_UP);
-
+	set_up_pins();
 
 	// Turn all LEDs off
 	for(i = 0; i < 8; i++) {
 		digitalWrite(i,LOW);
 	}
 
-
 	while(1) {
-	
-		for(i = 0; i < 8; i++) {
-			
-			if(i==0) {
-				digitalWrite(i,HIGH);
-				continue;
-			}
-	
-			delay(time_delay);
-
-			digitalWrite(i-1, LOW);
-			digitalWrite(i, HIGH);
+		if(direction == 1) {
+			moving_forward(time_delay);
 		}
-
-		
-		for(i = 7; i >= 0; i--) {
-			
-			if(i==0) {
-				digitalWrite(i,HIGH);
-				continue;
-			}
-	
-			delay(time_delay);
-
-			digitalWrite(i+1, LOW);
-			digitalWrite(i, HIGH);
+		if(direction == 2) {
+			moving_backward(time_delay);
 		}
 	}
 
+}
+void set_up_pins(void) {
+	int i;
+
+	wiringPiSetup();
+	
+	for(i = 0; i < 8; i++) {	
+		pinMode(i, OUTPUT);
+	}
+}
+
+
+void moving_forward(int time_delay) {
+	int i;
+	
+	digitalWrite(7,LOW);
+
+	for(i = 0; i < 8; i++) {
+			
+		if(i==0) {
+			digitalWrite(i,HIGH);
+			continue;
+		}
+	
+		delay(time_delay);
+
+		digitalWrite(i-1, LOW);
+		digitalWrite(i, HIGH);
+	}
+}
+
+void moving_backward(int time_delay) {
+	int i;
+
+	digitalWrite(0,LOW);
+
+	for(i = 7; i >= 0; i--) {
+			
+		if(i==7) {
+			digitalWrite(i,HIGH);
+			continue;
+		}
+	
+		delay(time_delay);
+
+		digitalWrite(i+1, LOW);
+		digitalWrite(i, HIGH);
+	}
 }
